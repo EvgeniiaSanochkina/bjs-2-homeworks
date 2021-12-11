b139b541df44739bbefc326847b437a37c8eded0
EvgeniiaSanochkina <sanochkina.es2501@gmail.com>
2021-12-12 00:28:12 +0700

    all done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,31 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+    let arr = {};
+    let D;
+    D = Math.pow(b, 2) - 4 * a * c;
+    if (D < 0) {
+        return false;
+    }
+    if (D == 0) {
+        arr = (-b / (2 * a));
+    }
+    if (D > 0) {
+        let res = [];
+        res.push((-b + Math.sqrt(D)) / (2 * a));
+        res.push((-b - Math.sqrt(D)) / (2 * a));
+        arr = res;
+    }
+
+    // код для задачи №1 писать здесь
+    return arr; // array
 }
 
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+    let totalAmount;
 
-  // код для задачи №2 писать здесь
+    // код для задачи №2 писать здесь
 
-  return totalAmount;
+    return totalAmount;
 }
