8d366126471ea88f76fae77ffb07c3c3be628895
EvgeniiaSanochkina <sanochkina.es2501@gmail.com>
2021-12-13 22:59:52 +0700

    changes

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -20,9 +20,29 @@ function solveEquation(a, b, c) {
     return arr; // array
 }
 
+"use strict";
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
     let totalAmount;
+    if (typeof percent != "string" || percent != "number") {
+        console.log("Параметр percent содержит неправильное значение percent")
+    };
+    if (typeof contribution != "string" || contribution != "number") {
+        console.log("Параметр contribution содержит неправильное значение contribution")
+    };
+    if (typeof amount != "string" || amount != "number") {
+        console.log("Параметр amount содержит неправильное значение amount")
+    };
+    if (typeof date != "object") {
+        console.log("Параметр date содержит неправильное значение date")
+    };
+    let loanBody = amount - contribution;
+    //сюда срок кредита 
+    let period;
+    let monthlyPercent = (percent / 12) / 100;
+    let monthlyPayment = loanBody * (monthlyPercent + (monthlyPercent / (Math.pow((1 + monthlyPercent), n) - 1)));
+    let totalAmount = monthlyPayment * period;
+    totalAmount.toFixed(2);
 
     // код для задачи №2 писать здесь
 
