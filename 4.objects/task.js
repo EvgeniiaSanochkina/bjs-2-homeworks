0c7c9c34ac979c39bc7236c9775024ccfa46131b
EvgeniiaSanochkina <sanochkina.es2501@gmail.com>
2021-12-29 23:16:06 +0700

    inprocess

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,36 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    console.log("name: " + name + "; gender: " + gender + "; age: " + age)
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+let student1 = new Student("Vova", "male", "18")
+
+
+Student.prototype.setSubject = function(subjectName) {
+    this.subject = subjectName;
+    console.log("subject: " + subjectName)
 }
 
-// ваш код для остальных методов
+student1.setSubject("Math");
+
+Student.prototype.addMark = function(mark) {
+
+    if (this.marks === undefined) {
+        this.marks = [mark];
+
+    } else {
+        this.marks = this.marks.push(mark);
+    }
+    console.log("marks: " + this.marks)
+}
+
+student1.addMark("10");
+student1.addMark("3")
+
+Student.prototype.addMarks = function(...mark) {
+    console.log("marks: " + mark)
+}
+
+student1.addMarks("2", "3", "5")
