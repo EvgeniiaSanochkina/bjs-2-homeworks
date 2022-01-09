7ed58c72e794dc179057cb76ff794f5522b2e456
EvgeniiaSanochkina <sanochkina.es2501@gmail.com>
2022-01-09 20:05:35 +0700

    in process

---
+++ 5.classes/task.js
@@ -0,0 +1,85 @@
+class PrintEditionItem {
+    constructor(name, releaseDate, pagesCount, state, type) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+        this.state = 100;
+        this.type = null;
+    }
+
+    fix() {
+        this.state = this.state * 1.5;
+        if (this.state > 100) {
+            this.state = 100;
+        }
+    }
+
+    set state(number) {
+        if (number < 0) {
+            this._state = 0;
+        }
+        if (number > 100) {
+            this._state = 100;
+        }
+        this._state = number;
+    }
+    get state() {
+        return this._state;
+    }
+}
+
+class Magazine extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount, state, type) {
+        super(name);
+        this.type = "magazine";
+    }
+}
+
+class Book extends PrintEditionItem {
+    constructor(author, name, releaseDate, pagesCount, state, type) {
+        super(name);
+        this.author = author;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+        this.type = "book";
+    }
+}
+
+class NovelBook extends Book {
+    constructor(name, releaseDate, pagesCount, state, type, author) {
+        super(name);
+        this.type = "novel";
+    }
+}
+
+class FantasticBook extends Book {
+    constructor(name, releaseDate, pagesCount, state, type, author) {
+        super(name);
+        this.type = "fantastic";
+    }
+}
+
+class DetectiveBook extends Book {
+    constructor(name, releaseDate, pagesCount, state, type, author) {
+        super(name);
+        this.type = "detective";
+    }
+}
+
+class Library extends PrintEditionItem {
+    constructor(name, books) {
+        super(name);
+        this.books = [];
+    }
+
+    addBook(book) {
+        if (this.state > 30) {
+            this.books.push(book);
+        }
+
+    }
+
+    findBookBy(type, value) {
+
+    }
+}
