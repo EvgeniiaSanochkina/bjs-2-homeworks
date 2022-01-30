f826ce76957ecaadc3171adb546dc7d798da70b1
EvgeniiaSanochkina <sanochkina.es2501@gmail.com>
2022-01-30 23:12:03 +0700

    almost

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,35 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+
+    let cache = [];
+
+    function wrapper(...args) {
+        const hash = args.join(",");
+
+        let idx = cache.findIndex((item) => item.hash === hash);
+
+        if (idx !== -1) {
+            console.log("Из кэша: " + cache[idx].value);
+            return "Из кэша: " + cache[idx].value;
+        } else {
+            let value = func(...args);
+            cache.push({ hash, value });
+            if (cache.length > 5) {
+                cache.shift();
+            }
+            console.log("Вычисляем: " + value);
+            return "Вычисляем: " + value;
+        }
+
+    }
+    return wrapper;
+
 }
 
 
 function debounceDecoratorNew(func) {
-  // Ваш код
+    // Ваш код
 }
 
 function debounceDecorator2(func) {
-  // Ваш код
+    // Ваш код
 }
